cf8c7e0a0e74b3dc237a9c1a1023980461aa8dbf
wh <wh@wh.com>
2018-01-02 16:27:37 +0800

    2018 1.2 1）添加上货完成的布局文件：close_door_list_item.xml（listView子布局）

--- shareiceboxms/src/main/java/com/example/shareiceboxms/views/fragments/CloseDoorFragment.java
+++ shareiceboxms/src/main/java/com/example/shareiceboxms/views/fragments/CloseDoorFragment.java
@@ -6,21 +6,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.shareiceboxms.R;
+import com.example.shareiceboxms.models.widget.ListViewForScrollView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CloseDoorFragment extends BaseFragment {
-
+    private View containerView;
+    private ListViewForScrollView productUpdateList;
+    private Button addMore, sure;
     private String callbackMsg;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_close_door, container, false);
+        containerView = inflater.inflate(R.layout.fragment_close_door, container, false);
+        return containerView;
     }
 
+    private void bindViews() {
+        productUpdateList = (ListViewForScrollView) containerView.findViewById(R.id.productUpdateList);
+        addMore = (Button) containerView.findViewById(R.id.addMore);
+        sure = (Button) containerView.findViewById(R.id.sure);
+    }
+
+    private void init() {
+        //adapter 中listview使用的子布局为：close_door_list_item.xml
+    }
+
+    private void bindListenner() {
+        addMore.setOnClickListener(this);
+        sure.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addMore:
+                break;
+            case R.id.sure:
+                break;
+        }
+    }
 }
