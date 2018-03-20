e0139d2ce7c856fff85cc1fb091240235cc6d118
liutianze <1048246748@qq.com>
2018-03-20 13:55:13 +0800

    咖啡机终端机项目搭建

---
+++ coffeeseller/src/main/java/example/jni/com/coffeeseller/views/activities/HomeActivity.java
@@ -0,0 +1,20 @@
+package example.jni.com.coffeeseller.views.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import example.jni.com.coffeeseller.R;
+
+/**
+ * Created by Administrator on 2018/3/20.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+}
